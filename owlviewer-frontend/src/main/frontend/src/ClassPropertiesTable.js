178a56cd51a9e5092067ef78107b4414b57ca752
Michael Grath <office@itmatters.at>
2021-04-23 04:09:23 +0200

    Replaced table component and implemented expandable rows

--- owlviewer-frontend/src/main/frontend/src/ClassPropertiesTable.js
+++ owlviewer-frontend/src/main/frontend/src/ClassPropertiesTable.js
@@ -1,32 +1,11 @@
 import React from "react";
 import createClass from 'create-react-class';
 import Table from "react-bootstrap/Table";
+import BootstrapTable from "react-bootstrap-table-next";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMinusSquare, faPlusSquare } from "@fortawesome/free-solid-svg-icons";
 import { hot } from "react-hot-loader";
 import axios from "axios";
-
-const TableRow = createClass({
-  render() {
-    return (
-      <tr>
-        <td>{this.props.row.name}</td>
-        <td style={{textAlign: "right"}}>{typeof this.props.row.types !== "undefined" ? (this.props.row.types.length > 0 ? (this.props.row.types[0].minCardinality === this.props.row.types[0].maxCardinality ? ("[" + (this.props.row.types[0].maxCardinality != null ? this.props.row.types[0].maxCardinality : "*") + "]") : ("[" + (this.props.row.types[0].minCardinality != null ? this.props.row.types[0].minCardinality : "*") + ".." + (this.props.row.types[0].maxCardinality != null ? this.props.row.types[0].maxCardinality : "*") + "]")): "") : ""}</td>
-        <td>{typeof this.props.row.types !== "undefined" ? (this.props.row.types.length > 0 ? (!this.props.row.literal ? <a href="#" role="button" onClick={() => {this.props.selectionHandler(this.props.row.types[0].iri)}}>{this.props.row.types[0].name}</a> : this.props.row.types[0].name) : "") : ""}</td>
-      </tr>
-    );
-  }
-});
-
-const TableBody = createClass({
-  render() {
-    return (
-      <tbody>
-        {this.props.data.map((row) => (
-          <TableRow selectionHandler={this.props.selectionHandler} row={row} />
-        ))}
-      </tbody>
-    );
-  }
-});
 
 class ClassPropertiesTable extends React.Component {
 
@@ -37,6 +16,10 @@ class ClassPropertiesTable extends React.Component {
   constructor(props) {
     super(props);
     this.getClassProperties = this.getClassProperties.bind(this);
+    this.getColumns = this.getColumns.bind(this);
+    this.cardinalityFormatter = this.cardinalityFormatter.bind(this);
+    this.propertyValueFormatter = this.propertyValueFormatter.bind(this);
+    this.getRowStyle = this.getRowStyle.bind(this);
   }
 
   getClassProperties(selectedNodeIRI) {
@@ -65,19 +48,123 @@ class ClassPropertiesTable extends React.Component {
     }
   }
 
+  cardinalityFormatter(cell, row) {
+    if (row.hasOwnProperty("types") && row.types.length > 0) {
+      var rows = [];
+      for (var typeIdx = 0; typeIdx < row.types.length; typeIdx++) {
+        if (row.types[typeIdx].minCardinality === row.types[typeIdx].maxCardinality) {
+          rows.push(<span>[{row.types[typeIdx].minCardinality}]<br/></span>);
+        }
+        else if (row.types[typeIdx].minCardinality != null && row.types[typeIdx].maxCardinality == null) {
+          rows.push(<span>[{row.types[typeIdx].minCardinality}..*]<br/></span>);
+        }
+        else if (row.types[typeIdx].minCardinality == null && row.types[typeIdx].maxCardinality != null) {
+          rows.push(<span>[*..{row.types[typeIdx].maxCardinality}]<br/></span>);
+        }
+        else if (row.types[typeIdx].minCardinality != null && row.types[typeIdx].maxCardinality != null) {
+          rows.push(<span>[{row.types[typeIdx].minCardinality}..{row.types[typeIdx].maxCardinality}]<br/></span>);
+        }
+        else {
+          rows.push(<span><br/></span>);
+        }
+      }
+      return <div>{rows}</div>
+    }
+  }
+
+  propertyValueFormatter(cell, row) {
+    if (row.hasOwnProperty("types") && row.types.length > 0) {
+      var rows = [];
+      for (var typeIdx = 0; typeIdx < row.types.length; typeIdx++) {
+          if (row.literal) {
+            rows.push(<span>{row.types[typeIdx].name}<br/></span>);
+          }
+          else {
+            var iri = row.types[typeIdx].iri;
+            rows.push(<a href="#" role="button" onClick={() => {this.props.selectionHandler(iri)}}>{row.types[typeIdx].name}<br/></a>);
+          }
+      }
+      return <div>{rows}</div>
+    }
+  }
+
+  getRowStyle(row, rowIndex) {
+    var style = {};
+    if (row.hasOwnProperty("types") && row.types.length > 1) {
+      style.lineHeight = "1.7rem";
+    }
+    return style;
+  }
+
+  getColumns() {
+    return [
+      {
+        dataField: "name",
+        text: "Property",
+        style: {
+          width: "45%"
+        },
+        sort: true
+      },
+      {
+        dataField: "cardinality",
+        text: "",
+        style: {
+          width: "10%"
+        },
+        align: "right",
+        formatter: this.cardinalityFormatter,
+      },
+      {
+        dataField: "types.name",
+        text: "Type",
+        style: {
+          width: "45%"
+        },
+        formatter: this.propertyValueFormatter
+      }
+    ];
+  }
+
   render() {
+    const expandRow = {
+      renderer: row => (
+        <div>
+          <p><b>Internationalised Resource Identifier (IRI):</b><br/>{row.iri}</p>
+          { row.description != null ? <p><b>Description:</b><br/>{row.description}</p> : "" }
+        </div>
+      ),
+      showExpandColumn: true,
+      expandHeaderColumnRenderer: ({ isAnyExpands }) => {
+        if (isAnyExpands) {
+          return <FontAwesomeIcon style={{color: "#949494"}} icon={faMinusSquare}/>;
+        }
+        return <FontAwesomeIcon style={{color: "#949494"}} icon={faPlusSquare}/>;
+      },
+      expandColumnRenderer: ({ expanded }) => {
+        if (expanded) {
+          return <FontAwesomeIcon style={{color: "#949494"}} icon={faMinusSquare}/>;
+        }
+        return <FontAwesomeIcon style={{color: "#949494"}} icon={faPlusSquare}/>;
+      }
+    }
+
+    const defaultSorted = [{
+      dataField: "name",
+      order: "asc"
+    }];
+
     return (
       <div className={this.props.className} style={this.props.style}>
-        <Table size="sm">
-          <thead>
-            <tr>
-              <th style={{width: "45%"}}>Property</th>
-              <th style={{width: "10%"}}/>
-              <th style={{width: "45%"}}>Type</th>
-            </tr>
-          </thead>
-          <TableBody selectionHandler={this.props.selectionHandler} data={this.state.properties} />
-        </Table>
+        <BootstrapTable
+          keyField="name"
+          data={this.state.properties}
+          columns={this.getColumns()}
+          bordered={false}
+          rowStyle={this.getRowStyle}
+          expandRow={expandRow}
+          defaultSorted={defaultSorted}
+          condensed/>
       </div>
     );
   }
